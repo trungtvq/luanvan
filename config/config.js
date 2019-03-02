5ca669282fae14b231630d0e794da84eaf81080b
minhtien1513457 <37267355+minhtien1513457@users.noreply.github.com>
2019-03-02 13:19:31 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports ={
-    db:'mongodb+srv://tien:111111111@overlead0-ykr4q.gcp.mongodb.net/admin',
-    db_dev:'mongodb+srv://tien:111111111@overlead0-ykr4q.gcp.mongodb.net/admin'
+    db:'mongodb+srv://tien:111111111@overlead0-ykr4q.gcp.mongodb.net/login_demo',
+    db_dev:'mongodb+srv://tien:111111111@overlead0-ykr4q.gcp.mongodb.net/login_demo'
 }
