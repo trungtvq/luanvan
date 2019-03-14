86d924e061932b408782404eabb8ce3902aa11b3
Trung <trung@MacBook-Pro.local>
2019-03-14 12:48:17 +0700

    add register, login, logout

--- client-overlead/src/views/Pages/Register/Register.js
+++ client-overlead/src/views/Pages/Register/Register.js
@@ -1,10 +1,221 @@
 import React, { Component } from 'react';
 import { Button, Card, CardBody, CardFooter, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
 
+import {
+  getFromStorage,
+  setInStorage,
+} from '../../../service/storage';
+
 class Register extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      token: '',
+      signUpError: '',
+      signInError: '',
+      signInEmail: '',
+      signInPassword: '',
+      signUpEmail: '',
+      signUpPassword: '',
+    };
+
+    this.onTextboxChangeSignInEmail = this.onTextboxChangeSignInEmail.bind(this);
+    this.onTextboxChangeSignInPassword = this.onTextboxChangeSignInPassword.bind(this);
+    this.onTextboxChangeSignUpEmail = this.onTextboxChangeSignUpEmail.bind(this);
+    this.onTextboxChangeSignUpPassword = this.onTextboxChangeSignUpPassword.bind(this);
+    
+    this.onSignIn = this.onSignIn.bind(this);
+    this.onSignUp = this.onSignUp.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  componentDidMount() {
+    const obj = getFromStorage('the_main_app');
+    if (obj && obj.token) {
+      const { token } = obj;
+      // Verify token
+      fetch('/api/account/verify?token=' + token)
+        .then(res => res.json())
+        .then(json => {
+          if (json.success) {
+            this.setState({
+              token,
+              isLoading: false
+            });
+          } else {
+            this.setState({
+              isLoading: false,
+            });
+          }
+        });
+    } else {
+      this.setState({
+        isLoading: false,
+      });
+    }
+  }
+
+  onTextboxChangeSignInEmail(event) {
+    this.setState({
+      signInEmail: event.target.value,
+    });
+  }
+
+  onTextboxChangeSignInPassword(event) {
+    this.setState({
+      signInPassword: event.target.value,
+    });
+  }
+
+  onTextboxChangeSignUpEmail(event) {
+    this.setState({
+      signUpEmail: event.target.value,
+    });
+  }
+
+  onTextboxChangeSignUpPassword(event) {
+    this.setState({
+      signUpPassword: event.target.value,
+    });
+  }
+
+  onSignUp() {
+    // Grab state
+    const {
+      signUpEmail,
+      signUpPassword,
+    } = this.state;
+
+    this.setState({
+      isLoading: true,
+    });
+
+    // Post request to backend
+    fetch('/api/account/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: signUpEmail,
+        password: signUpPassword,
+      }),
+    }).then(res => res.json())
+      .then(json => {
+        console.log('json', json);
+        if (json.success) {
+          this.setState({
+            signUpError: json.message,
+            isLoading: false,
+            signUpEmail: '',
+            signUpPassword: '',
+          });
+        } else {
+          this.setState({
+            signUpError: json.message,
+            isLoading: false,
+          });
+        }
+      });
+  }
+
+  onSignIn() {
+    // Grab state
+    const {
+      signInEmail,
+      signInPassword,
+    } = this.state;
+
+    this.setState({
+      isLoading: true,
+    });
+
+    // Post request to backend
+    fetch('/api/account/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: signInEmail,
+        password: signInPassword,
+      }),
+    }).then(res => res.json())
+      .then(json => {
+        console.log('json', json);
+        if (json.success) {
+          setInStorage('the_main_app', { token: json.token });
+          this.setState({
+            signInError: json.message,
+            isLoading: false,
+            signInPassword: '',
+            signInEmail: '',
+            token: json.token,
+          });
+        } else {
+          this.setState({
+            signInError: json.message,
+            isLoading: false,
+          });
+        }
+      });
+  }
+
+  logout() {
+    this.setState({
+      isLoading: true,
+    });
+    const obj = getFromStorage('the_main_app');
+    if (obj && obj.token) {
+      const { token } = obj;
+      // Verify token
+      fetch('/api/account/logout?token=' + token)
+        .then(res => res.json())
+        .then(json => {
+          if (json.success) {
+            this.setState({
+              token: '',
+              isLoading: false
+            });
+          } else {
+            this.setState({
+              isLoading: false,
+            });
+          }
+        });
+    } else {
+      this.setState({
+        isLoading: false,
+      });
+    }
+  }
+
   render() {
+        const {
+      isLoading,
+      token,
+      signInError,
+      signInEmail,
+      signInPassword,
+      signUpEmail,
+      signUpPassword,
+      signUpError,
+    } = this.state;
+
+    if (isLoading) {
+      return (<div><p>Loading...</p></div>);
+    }
+
+    if (!token) {
     return (
       <div className="app flex-row align-items-center">
+      {
+              (signUpError) ? (
+                <p>{signUpError}</p>
+              ) : (null)
+            }
         <Container>
           <Row className="justify-content-center">
             <Col md="9" lg="7" xl="6">
@@ -19,7 +230,8 @@ class Register extends Component {
                           <i className="icon-user"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="text" placeholder="Username" autoComplete="username" />
+                      <Input type="text" placeholder="Username" autoComplete="username" value={signUpEmail}
+              onChange={this.onTextboxChangeSignUpEmail} />
                     </InputGroup>
                     <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
@@ -33,7 +245,8 @@ class Register extends Component {
                           <i className="icon-lock"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="password" placeholder="Password" autoComplete="new-password" />
+                      <Input type="password" placeholder="Password" autoComplete="new-password" value={signUpPassword}
+              onChange={this.onTextboxChangeSignUpPassword}/>
                     </InputGroup>
                     <InputGroup className="mb-4">
                       <InputGroupAddon addonType="prepend">
@@ -43,7 +256,7 @@ class Register extends Component {
                       </InputGroupAddon>
                       <Input type="password" placeholder="Repeat password" autoComplete="new-password" />
                     </InputGroup>
-                    <Button color="success" block>Create Account</Button>
+                    <Button color="success" block onClick={this.onSignUp}>Create Account</Button>
                   </Form>
                 </CardBody>
                 <CardFooter className="p-4">
@@ -63,6 +276,13 @@ class Register extends Component {
       </div>
     );
   }
-}
+
+  return (
+    <div>
+      <p>Account</p>
+      <button onClick={this.logout}>Logout</button>
+    </div>
+  );
+  }}
 
 export default Register;
