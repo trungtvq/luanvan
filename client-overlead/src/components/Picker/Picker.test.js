d08375c440ee038e422e74268fe37fe10c734b59
Trung <trung@Trungs-MacBook-Pro.local>
2019-03-16 17:26:57 +0700

    del client

--- client-overlead/src/components/Picker/Picker.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Picker from './Picker';
-import { mount } from 'enzyme'
-
-it('renders without crashing', () => {
-  mount(<Picker />);
-});
--- client-overlead/src/components/Posts/Post.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Posts from './Posts';
-import { mount } from 'enzyme'
-
-it('renders without crashing', () => {
-  mount(<Posts />);
-});
