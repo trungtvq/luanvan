f5ac97dbf72966e064f9204e91d5c3c032bcfa9a
Trung <trung@Trungs-MacBook-Pro.local>
2019-03-16 17:57:47 +0700

    test auto deploy heroku

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,9 @@ router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
   res.json('respond with a resource');
 });
+router.get('/1', function(req, res, next) {
+  //res.send('respond with a resource');
+  res.json('you get first user');
+});
 
 module.exports = router;
