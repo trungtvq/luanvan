f2ab0d379115133ea7395e23ce888ae111d7b444
Trung <trung@Trungs-MacBook-Pro.local>
2019-03-17 00:35:43 +0700

    add mail

--- routes/users1.js
+++ routes/users1.js
@@ -8,7 +8,18 @@ router.get('/', function(req, res, next) {
 });
 router.get('/1', function(req, res, next) {
   //res.send('respond with a resource');
-  res.json('you get first user');
+  const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
+  console.log(process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: 'test@example.com',
+    from: 'test@example.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
+  res.json('email were send');
 });
 
 module.exports = router;
