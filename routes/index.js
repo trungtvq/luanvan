f0b9ecd63102849e10d8e3d2b3ea2890bcf0767e
Trung <trung@Trungs-MacBook-Pro.local>
2019-03-17 22:06:30 +0700

    this is a spam

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express has change5' });
+  res.render('index', { title: 'Express has change511' });
 });
 
 module.exports = router;
