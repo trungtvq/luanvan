e3d6fbf4eceb76111388aa1b35518a87ee939556
Trung <trung@Trungs-MacBook-Pro.local>
2019-03-17 20:19:06 +0700

    check update again

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express has change2' });
+  res.render('index', { title: 'Express has change3' });
 });
 
 module.exports = router;
