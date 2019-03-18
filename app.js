580ef704ada425964a80998d320b6b48124f946f
Trung <trung@Trungs-MacBook-Pro.local>
2019-03-18 15:35:50 +0700

    fix link of api

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/', indexRouter);
 app.use('/api/users/1', usersRouter);
 app.use('/api/users1', users1Router);
-app.use('/api/api/account',accountRouter);
+app.use('/api/account',accountRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
