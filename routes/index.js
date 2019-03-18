78fd09f1a83f2bdda52b495b9d668297073d07ed
Trung <trung@Trungs-MacBook-Pro.local>
2019-03-18 12:42:41 +0700

    vps

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express has change511' });
+  res.render('index', { title: 'Provide api for app' });
 });
 
 module.exports = router;
