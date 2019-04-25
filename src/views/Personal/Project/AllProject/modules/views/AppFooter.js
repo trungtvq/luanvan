5f1aace467b67615e8d68b2dd22dbc91475cc2ce
minhtien1513457 <37267355+minhtien1513457@users.noreply.github.com>
2019-04-25 20:01:27 +0700

    noproject page

---
+++ src/views/Personal/Project/AllProject/modules/views/AppFooter.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import compose from 'recompose/compose';
+import pure from 'recompose/pure';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import LayoutBody from '../components/LayoutBody';
+import Typography from '../components/Typography';
+import TextField from '../components/TextField';
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    backgroundColor: theme.palette.secondary.light,
+  },
+  layoutBody: {
+    marginTop: theme.spacing.unit * 8,
+    marginBottom: theme.spacing.unit * 8,
+    display: 'flex',
+  },
+  iconsWrapper: {
+    height: 120,
+  },
+  icons: {
+    display: 'flex',
+  },
+  icon: {
+    width: 48,
+    height: 48,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    //backgroundColor: "#f7f7f7",//zzz
+    marginRight: theme.spacing.unit,
+    '&:hover': {
+      backgroundColor: "#d8d4d6",
+    },
+  },
+  list: {
+    margin: 0,
+    listStyle: 'none',
+    paddingLeft: 0,
+  },
+  listItem: {
+    paddingTop: theme.spacing.unit / 2,
+    paddingBottom: theme.spacing.unit / 2,
+  },
+  
+});
+
+
+function AppFooter(props) {
+  const { classes } = props;
+
+  return (
+    <Typography component="footer" className={classes.root}>
+      <LayoutBody className={classes.layoutBody} width="large">
+        <Grid container spacing={10}>
+          <Grid item xs={6} sm={4} md={2}>
+            <Grid
+              container
+              direction="column"
+              justify="flex-end"
+              className={classes.iconsWrapper}
+              spacing={16}
+            >
+              <Grid item className={classes.icons}>
+                <a href="https://overlead.co/" className={classes.icon}>
+                  <img src="/Home/appFooterFacebook.png" alt="Facebook" />
+                </a>
+                <a href="https://twitter.com/overlead" className={classes.icon}>
+                  <img src="/Home/appFooterTwitter.png" alt="Twitter" />
+                </a>
+              </Grid>
+              <Grid item>© 2019 Overlead</Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={6} sm={4} md={2}>
+            <Typography variant="h6" marked="left" gutterBottom>
+              Legal
+            </Typography>
+            <ul className={classes.list}>
+              <li className={classes.listItem}>
+                <Link href="/premium-themes/onepirate/terms">Terms</Link>
+              </li>
+              <li className={classes.listItem}>
+                <Link href="/premium-themes/onepirate/privacy">Privacy</Link>
+              </li>
+            </ul>
+          </Grid>
+          <Grid item xs={6} sm={8} md={4}>
+            
+          </Grid>
+          <Grid item>
+            <Typography variant="caption">
+              
+            </Typography>
+          </Grid>
+        </Grid>
+      </LayoutBody>
+    </Typography>
+  );
+}
+
+AppFooter.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default compose(
+  pure,
+  withStyles(styles),
+)(AppFooter);
